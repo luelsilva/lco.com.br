03ae8057820e4d22824ad682398a7008eb670fb8
luelsilva <luizkrx@gmail.com>
2022-05-30 16:09:54 -0300

    updade

---
+++ testes/zoom/script.js
@@ -0,0 +1,50 @@
+var scale = 1,
+  panning = false,
+  pointX = 0,
+  pointY = 0,
+  start = { x: 0, y: 0 };
+
+function init() {
+  var zoom = document.getElementById("zoom");
+  zoom.onmousedown = mousedown_handler;
+  zoom.onmouseup = mouseup_handler;
+  zoom.onmousemove = mousemode_handler;
+  zoom.onwheel = mousewhell_handler;
+}
+
+function setTransform() {
+  zoom.style.transform =
+    "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+}
+
+function mousedown_handler(e) {
+  e.preventDefault();
+  start = { x: e.clientX - pointX, y: e.clientY - pointY };
+  panning = true;
+}
+
+function mouseup_handler(e) {
+  panning = false;
+}
+
+function mousemode_handler(e) {
+  e.preventDefault();
+  if (!panning) {
+    return;
+  }
+  pointX = e.clientX - start.x;
+  pointY = e.clientY - start.y;
+  setTransform();
+}
+
+function mousewhell_handler(e) {
+  e.preventDefault();
+  var xs = (e.clientX - pointX) / scale,
+    ys = (e.clientY - pointY) / scale,
+    delta = e.wheelDelta ? e.wheelDelta : -e.deltaY;
+  delta > 0 ? (scale *= 1.2) : (scale /= 1.2);
+  pointX = e.clientX - xs * scale;
+  pointY = e.clientY - ys * scale;
+
+  setTransform();
+}
