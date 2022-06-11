877814c0c59b679130a249a2df60f5d4fa385df1
luelsilva <luizkrx@gmail.com>
2022-06-11 19:34:24 -0300

    novo teste
    este novo teste vai inicializar pelo evento do pointer e depois avançaremos para o mouse

--- testes/zoom/script.js
+++ testes/zoom/script.js
@@ -1,164 +1,113 @@
-
+// Global vars to cache event state
 var evCache = new Array();
 var prevDiff = -1;
 
-var scale = 1,
-    panning = false,
-    pointX = 0,
-    pointY = 0,
-    start = { x: 0, y: 0 },
-    imgNatWidth = 0,
-    imgNatHeigth = 0;
+function init() {
+    // Install event handlers for the pointer target
+    var el=document.getElementById("target");
+    el.onpointerdown = pointerdown_handler;
+    el.onpointermove = pointermove_handler;
+   
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    el.onpointerup = pointerup_handler;
+    el.onpointercancel = pointerup_handler;
+    el.onpointerout = pointerup_handler;
+    el.onpointerleave = pointerup_handler;
+   }
 
-function inicializa() {
-    zoom = document.getElementById("zoom");
-    zoom.onmouseup = mouseup_handler;
-    zoom.onmousedown = mousedown_handler;
-    zoom.onmousemove = mousemove_handler;
-    zoom.onwheel = mousewheel_handler;
-
-    zoom.onpointerdown = pointerdown_handler;
-    zoom.onpointerup = pointerup_handler;
-    zoom.onpointermove = pointermove_handler;
-
-    logx("versão 14 <br/>");
-};
-
-function imgLoad() {
-    imgzoom = document.getElementById("imgzoom");
-    imgNatWidth = imgzoom.naturalWidth;
-    imgNatHeigth = imgzoom.naturalHeight;
-};
-
-function logx(texto) {
-    output = document.getElementsByTagName('output')[0];
-    output.innerHTML = texto;
-};
-
-function log(texto) {
-    var o = document.getElementsByTagName('output')[0];
-    o.innerHTML += texto + " ";
-};
-
-
-function setTransform() {
-    zoom.style.transform =
-        "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
-};
-
-function remove_event(ev) {
-    // Remove this event from the target's cache
-    for (var i = 0; i < evCache.length; i++) {
-        if (evCache[i].pointerId == ev.pointerId) {
-            evCache.splice(i, 1);
-            break;
-        }
-    }
-}
-
-function mousedown_handler(e) {
-    e.preventDefault();
-    start = { x: e.clientX - pointX, y: e.clientY - pointY };
-    panning = true;
-};
-
-function pointerdown_handler(e) {
-    //e.preventDefault();
-    start = { x: e.clientX - pointX, y: e.clientY - pointY };
-    panning = true;
-    evCache.push(e);
-    log("pointerdown_handler");
-};
-
-function pointerup_handler(e) {
-    //e.preventDefault();
-    panning = false;
-
-    remove_event(e);
-
-    // If the number of pointers down is less than two then reset diff tracker
-    if (evCache.length < 2) {
-        prevDiff = -1;
-    }
-    log("pointerup_handler");
-}
-
-function mouseup_handler(e) {
-    e.preventDefault();
-    panning = false;
-};
-
-function mousewheel_handler(e) {
-    e.preventDefault();
-    var xs = (e.clientX - pointX) / scale,
-        ys = (e.clientY - pointY) / scale,
-        delta = e.wheelDelta ? e.wheelDelta : -e.deltaY;
-    delta > 0 ? (scale *= 1.2) : (scale /= 1.2);
-
-    pointX = e.clientX - xs * scale;
-    pointY = e.clientY - ys * scale;
-
-    setTransform();
-};
-
-function mousemove_handler(e) {
-    e.preventDefault();
-    if (!panning) {
-        return;
-    }
-    pointX = e.clientX - start.x;
-    pointY = e.clientY - start.y;
-
-    setTransform();
-};
-
-function pointermove_handler(ev) {
-    e.preventDefault();
-
-    log("PM 1")
-
+   function pointerdown_handler(ev) {
+    // The pointerdown event signals the start of a touch interaction.
+    // This event is cached to support 2-finger gestures
+    evCache.push(ev);
+    log("pointerDown", ev);
+   }
+   
+   function pointermove_handler(ev) {
+    // This function implements a 2-pointer horizontal pinch/zoom gesture.
+    //
+    // If the distance between the two pointers has increased (zoom in),
+    // the target element's background is changed to "pink" and if the
+    // distance is decreasing (zoom out), the color is changed to "lightblue".
+    //
+    // This function sets the target element's border to "dashed" to visually
+    // indicate the pointer's target received a move event.
+    log("pointerMove", ev);
+    ev.target.style.border = "dashed";
+   
     // Find this event in the cache and update its record with this event
     for (var i = 0; i < evCache.length; i++) {
-        if (ev.pointerId == evCache[i].pointerId) {
-            evCache[i] = ev;
-            break;
-        }
+      if (ev.pointerId == evCache[i].pointerId) {
+         evCache[i] = ev;
+      break;
+      }
     }
-
+   
     // If two pointers are down, check for pinch gestures
     if (evCache.length == 2) {
-        // Calculate the distance between the two pointers
-        var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-
-        if (prevDiff > 0) {
-            if (curDiff > prevDiff) {
-                // The distance between the two pointers has increased
-                log("Pinch moving OUT -> Zoom in", ev);
-                ev.target.style.background = "pink";
-            }
-            if (curDiff < prevDiff) {
-                // The distance between the two pointers has decreased
-                log("Pinch moving IN -> Zoom out", ev);
-                ev.target.style.background = "lightblue";
-            }
+      // Calculate the distance between the two pointers
+      var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+   
+      if (prevDiff > 0) {
+        if (curDiff > prevDiff) {
+          // The distance between the two pointers has increased
+          log("Pinch moving OUT -> Zoom in", ev);
+          ev.target.style.background = "pink";
         }
-
-        // Cache the distance for the next move event
-        prevDiff = curDiff;
-        log("PM 2")
-    }
-
-    else {
-        if (!panning) {
-            return;
+        if (curDiff < prevDiff) {
+          // The distance between the two pointers has decreased
+          log("Pinch moving IN -> Zoom out",ev);
+          ev.target.style.background = "lightblue";
         }
-        pointX = e.clientX - start.x;
-        pointY = e.clientY - start.y;
-
-        setTransform();
-        log("PM 3")
+      }
+   
+      // Cache the distance for the next move event
+      prevDiff = curDiff;
     }
+   }
+   
+   function pointerup_handler(ev) {
+    log(ev.type, ev);
+    // Remove this pointer from the cache and reset the target's
+    // background and border
+    remove_event(ev);
+    ev.target.style.background = "white";
+    ev.target.style.border = "1px solid black";
+  
+    // If the number of pointers down is less than two then reset diff tracker
+    if (evCache.length < 2) {
+      prevDiff = -1;
+    }
+  }
+  
+  function remove_event(ev) {
+    // Remove this event from the target's cache
+    for (var i = 0; i < evCache.length; i++) {
+      if (evCache[i].pointerId == ev.pointerId) {
+        evCache.splice(i, 1);
+        break;
+      }
+    }
+   }
 
+   // Log events flag
+var logEvents = false;
 
+// Logging/debugging functions
+function enableLog(ev) {
+  logEvents = logEvents ? false : true;
+}
 
+function log(prefix, ev) {
+  if (!logEvents) return;
+  var o = document.getElementsByTagName('output')[0];
+  var s = prefix + ": pointerID = " + ev.pointerId +
+                " ; pointerType = " + ev.pointerType +
+                " ; isPrimary = " + ev.isPrimary;
+  o.innerHTML += s + " ";
+}
+
+function clearLog(event) {
+ var o = document.getElementsByTagName('output')[0];
+ o.innerHTML = "";
 }
