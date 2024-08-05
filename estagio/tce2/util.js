20aa91e0714694f748c3b8c1fe072f4576b1060e
Luiz Carlos Oliveira da Silva <45966334+luelsilva@users.noreply.github.com>
2024-08-05 11:28:29 -0300

    up

--- estagio/tce2/util.js
+++ estagio/tce2/util.js
@@ -161,3 +161,34 @@ async function getCursos() {
     alert("Erro ao buscar dados do curso");
   }
 };
+
+
+// carregar dados de um arquivo json para um form
+document.getElementById('loadBtn').addEventListener('click', function () {
+  document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      try {
+        const data = JSON.parse(e.target.result);
+        const form = document.getElementById('myForm');
+
+        for (const key in data) {
+          if (data.hasOwnProperty(key)) {
+            const input = form.querySelector(`[name="${key}"]`);
+            if (input) {
+              input.value = data[key];
+            }
+          }
+        }
+      } catch (error) {
+        alert('Erro ao processar o arquivo JSON: ' + error.message);
+      }
+    };
+    reader.readAsText(file);
+  }
+});
