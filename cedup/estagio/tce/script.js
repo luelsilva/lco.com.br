9b3e9177bcd6590df6f864b68db59b9ffbd9e3db
Luiz Carlos Oliveira da Silva <45966334+luelsilva@users.noreply.github.com>
2024-08-11 22:36:38 -0300

    up

--- cedup/estagio/tce/script.js
+++ cedup/estagio/tce/script.js
@@ -4,6 +4,7 @@ const cursoURL = 'https://www.lco.com.br/cedup/estagio/assets/cursos_tecnicos.cs
 // carrega cursos no form select 'siglaCurso'
 document.addEventListener("DOMContentLoaded", function () {
   // carrega os curso no input form select 'siglaCurso'
+
   fetch(cursoURL)
     .then(response => response.text())
     .then(data => {
@@ -311,51 +312,63 @@ async function getCursoByID(siglaCurso) {
 
 async function imprimir(formObject) {
 
-  // Seleciona a div print
-  const divPrint = document.getElementById('print');
+  const siglaCurso = formObject["siglaCurso"];
 
-  // Substitui os placeholders com os valores do objeto JSON
-  divPrint.innerHTML = divPrint.innerHTML
-    .replace('${nomeEstagiario}', formObject["nomeEstagiario"])
-    .replace('${nomeCurso}', formObject["nomeCurso"])
-    .replace('${matriculaEstagiario}', formObject["matriculaEstagiario"])
-    .replace('${cpfEstagiario}', formObject["cpfEstagiario"])
-    .replace('${telefoneEstagiario}', formObject["telefoneEstagiario"])
-    .replace('${emailEstagiario}', formObject["emailEstagiario"])
-    .replace('${dataNascimento}', await formataData(formObject["dataNascimento"]))
-    .replace('${enderecoCompletoEstagiario}', formObject["enderecoCompletoEstagiario"])
-    .replace('${deficiencia}', formObject["deficiencia"])
-    .replace('${nomeProfessor}', formObject["nomeProfessor"])
-    .replace('${matriculaProfessor}', formObject["matriculaProfessor"])
-    .replace('${emailProfessor}', formObject["emailProfessor"])
-    .replace('${nomeEmpresa}', formObject["nomeEmpresa"])
-    .replace('${cnpj}', formObject["cnpj"])
-    .replace('${enderecoCompletoEmpresa}', formObject["enderecoCompletoEmpresa"])
-    .replace('${telefoneEmpresa}', formObject["telefoneEmpresa"])
-    .replace('${emailEmpresa}', formObject["emailEmpresa"])
-    .replace('${ramoAtividade}', formObject["ramoAtividade"])
-    .replace('${representante}', formObject["representante"])
-    .replace('${supervidor}', formObject["supervidor"])
-    .replace('${cargo}', formObject["cargo"])
-    .replace('${apolice}', formObject["apolice"])
-    .replace('${seguradora}', formObject["seguradora"])
-    .replace('${cnpjSeguradora}', formObject["cnpjSeguradora"])
-    .replace('${dataInicio}', await formataData(formObject["dataInicio"]))
-    .replace('${dataFim}', await formataData(formObject["dataFim"]))
-    .replace('${dataAtual}', formObject["dataAtual"])
-    .replace('${nomeEstagiario2}', formObject["nomeEstagiario"])
-    .replace('${cpfEstagiario2}', formObject["cpfEstagiario"])
-    .replace('${tipoEstagio}', formObject["tipoEstagio"])
-    .replace('${dataInicio2}', await formataData(formObject["dataInicio"]))
-    .replace('${dataFim2}', await formataData(formObject["dataFim"]))
-    .replace('${horarioEstagio}', formObject["horarioEstagio"].replace(/\n/g, "<br>"))
-    .replace('${cargaHorariaSemanal}', formObject["cargaHorariaSemanal"])
-    .replace('${cargaHorariaTotal}', formObject["cargaHorariaTotal"])
-    .replace('${apolice2}', formObject["apolice"])
-    .replace('${bolsaAuxilio}', formObject["bolsaAuxilio"].replace(/\n/g, "<br>"))
-    .replace('${atividades}', formObject["atividades"].replace(/\n/g, "<br>"))
+  var result = await getCursoByID(siglaCurso);
 
-  window.print();
+  if (result) {
+
+    formObject["nomeCurso"] = result[1];
+    formObject["matriculaProfessor"] = result[2];
+    formObject["nomeProfessor"] = result[3];
+    formObject["emailProfessor"] = result[4];
+
+    // Seleciona a div print
+    const divPrint = document.getElementById('print');
+
+    // Substitui os placeholders com os valores do objeto JSON
+    divPrint.innerHTML = divPrint.innerHTML
+      .replace('${nomeEstagiario}', formObject["nomeEstagiario"])
+      .replace('${nomeCurso}', formObject["nomeCurso"])
+      .replace('${matriculaEstagiario}', formObject["matriculaEstagiario"])
+      .replace('${cpfEstagiario}', formObject["cpfEstagiario"])
+      .replace('${telefoneEstagiario}', formObject["telefoneEstagiario"])
+      .replace('${emailEstagiario}', formObject["emailEstagiario"])
+      .replace('${dataNascimento}', await formataData(formObject["dataNascimento"]))
+      .replace('${enderecoCompletoEstagiario}', formObject["enderecoCompletoEstagiario"])
+      .replace('${deficiencia}', formObject["deficiencia"])
+      .replace('${nomeProfessor}', formObject["nomeProfessor"])
+      .replace('${matriculaProfessor}', formObject["matriculaProfessor"])
+      .replace('${emailProfessor}', formObject["emailProfessor"])
+      .replace('${nomeEmpresa}', formObject["nomeEmpresa"])
+      .replace('${cnpj}', formObject["cnpj"])
+      .replace('${enderecoCompletoEmpresa}', formObject["enderecoCompletoEmpresa"])
+      .replace('${telefoneEmpresa}', formObject["telefoneEmpresa"])
+      .replace('${emailEmpresa}', formObject["emailEmpresa"])
+      .replace('${ramoAtividade}', formObject["ramoAtividade"])
+      .replace('${representante}', formObject["representante"])
+      .replace('${supervidor}', formObject["supervidor"])
+      .replace('${cargo}', formObject["cargo"])
+      .replace('${apolice}', formObject["apolice"])
+      .replace('${seguradora}', formObject["seguradora"])
+      .replace('${cnpjSeguradora}', formObject["cnpjSeguradora"])
+      .replace('${dataInicio}', await formataData(formObject["dataInicio"]))
+      .replace('${dataFim}', await formataData(formObject["dataFim"]))
+      .replace('${dataAtual}', formObject["dataAtual"])
+      .replace('${nomeEstagiario2}', formObject["nomeEstagiario"])
+      .replace('${cpfEstagiario2}', formObject["cpfEstagiario"])
+      .replace('${tipoEstagio}', formObject["tipoEstagio"])
+      .replace('${dataInicio2}', await formataData(formObject["dataInicio"]))
+      .replace('${dataFim2}', await formataData(formObject["dataFim"]))
+      .replace('${horarioEstagio}', formObject["horarioEstagio"].replace(/\n/g, "<br>"))
+      .replace('${cargaHorariaSemanal}', formObject["cargaHorariaSemanal"])
+      .replace('${cargaHorariaTotal}', formObject["cargaHorariaTotal"])
+      .replace('${apolice2}', formObject["apolice"])
+      .replace('${bolsaAuxilio}', formObject["bolsaAuxilio"].replace(/\n/g, "<br>"))
+      .replace('${atividades}', formObject["atividades"].replace(/\n/g, "<br>"))
+
+    window.print();
+  }
 }
 
 // Adiciona um event listener para o evento de submissão do formulário
@@ -404,22 +417,8 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
 
   formObject["dataAtual"] = getDataAtual();
 
-  const siglaCurso = formObject["siglaCurso"];
+  imprimir(formObject);
 
-  var result = getCursoByID(siglaCurso);
-
-  if (result) {
-
-    formObject["nomeCurso"] = result[1];
-    formObject["matriculaProfessor"] = result[2];
-    formObject["nomeProfessor"] = result[3];
-    formObject["emailProfessor"] = result[4];
-
-    // let valores = JSON.stringify(formObject);
-    //console.log(valores);
-
-    imprimir(formObject);
-  }
 });
 
 
