e999a120fb94c1bcf81966c5041b4401af58c894
Luiz Carlos Oliveira da Silva <45966334+luelsilva@users.noreply.github.com>
2024-09-20 10:04:52 -0300

    up

--- cedup/estagio/carta/script.js
+++ cedup/estagio/carta/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const linhas = data.split('\n');
       // Ignora a primeira linha
       const linhasSemPrimeira = linhas.slice(1);
-      const cursoSelect = document.getElementById('siglaCurso');
+      const cursoSelect = document.getElementById('curso');
       linhasSemPrimeira.forEach(linha => {
         const colunas = linha.split(',');
         if (colunas.length >= 2) {
@@ -25,217 +25,52 @@ document.addEventListener("DOMContentLoaded", function () {
     alert('Erro ao carregar os cursos');
 });
 
-// onClick do elemento loadBtn
-document.getElementById('loadBtn').addEventListener('click', function () {
-  document.getElementById('fileInput').click();
+// Seleciona o formulário pelo ID
+const form = document.getElementById('formCarta');
+
+// Adiciona um event listener para o evento de submissão do formulário
+form.addEventListener('submit', function (event) {
+  // Impede o envio do formulário
+  event.preventDefault();
+
+  // Chama a função desejada
+  imprimir();
 });
 
-// onChange do elemento fileInput
-document.getElementById('fileInput').addEventListener('change', function (event) {
-  const file = event.target.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      try {
-        const data = JSON.parse(e.target.result);
-        const form = document.getElementById('myForm');
-        for (const key in data) {
-          if (data.hasOwnProperty(key)) {
-            const input = form.querySelector(`[name="${key}"]`);
-            if (input) {
-              input.value = data[key];
-            }
-          }
-        }
-      } catch (error) {
-        alert('Erro ao processar o arquivo JSON: ' + error.message);
-      }
-    };
-    reader.readAsText(file);
+async function imprimir() {
+
+  var nomeAluno = document.getElementById('nomeAluno').value.toUpperCase();
+  var matriculaAluno = document.getElementById('matriculaAluno').value;
+  var cargaHoraria = document.getElementById('cargaHoraria').value;
+  var curso = document.getElementById('curso').value;
+
+  /*
+  if (!nomeAluno || !matriculaAluno || !cargaHoraria || !curso) {
+      return;
   }
-});
+*/
 
-// salva o form
-document.getElementById('saveBtn').addEventListener('click', function () {
+  document.getElementById('print_nome_aluno').innerHTML = nomeAluno;
+  document.getElementById('print_matricula_aluno').innerHTML = document.getElementById('matriculaAluno').value;
 
-  const form = document.getElementById('myForm');
+  var selectElement = document.getElementById("curso");
+  var selectedIndex = selectElement.selectedIndex;
+  var selectedText = selectElement.options[selectedIndex].text;
 
-  // Cria um objeto FormData a partir do formulário
-  const formData = new FormData(form);
+  document.getElementById('print_nome_curso').innerHTML = selectedText;
+  document.getElementById('print_carga_horaria').innerHTML = document.getElementById('cargaHoraria').value;
+  document.getElementById('print_data_atual').innerHTML = "Joinville, " + getDataAtual();
 
-  // Inicializa um objeto vazio para armazenar os dados do formulário
-  const formDataJson = {};
+  var result = await buscarDados();
 
-  // Itera sobre os pares chave/valor do FormData e adiciona ao objeto JSON
-  formData.forEach((value, key) => {
-    // Se a chave já existe, converte o valor em um array (para campos com o mesmo nome)
-    if (formDataJson[key]) {
-      if (Array.isArray(formDataJson[key])) {
-        formDataJson[key].push(value);
-      } else {
-        formDataJson[key] = [formDataJson[key], value];
-      }
-    } else {
-      formDataJson[key] = value;
-    }
-  });
+  if (result) {
+    document.getElementById('print_nome_professor').innerHTML = "Dados do orientador de estágio: " + result[3];
+    document.getElementById('print_matricula_professor').innerHTML = "Matrícula: " + result[2];
+    document.getElementById('print_email_professor').innerHTML = "E-mail: " + result[4];
 
-  let fileName = 'Cedup-TCE-' + formDataJson['nomeEstagiario'];
-
-  const jsonString = JSON.stringify(formDataJson, null, 2); // Converter objeto JSON para string
-  const blob = new Blob([jsonString], { type: "application/json" }); // Criar um Blob com o conteúdo JSON
-  const url = URL.createObjectURL(blob);  // Criar uma URL para o Blob
-
-  // isso aqui é porque o chrome não abre o diálogo
-  fileName = prompt("Entre o nome do arquivo para salvar:", fileName);
-
-  if (fileName) {
-    fileName = fileName + '.json';
-    const a = document.createElement("a");  // Criar um elemento de link
-    a.href = url;
-    a.download = fileName;                  // Nome sugerido para o arquivo
-    document.body.appendChild(a);           // Adicionar o link ao DOM
-    a.click();                              // clicar nele para abrir a janela de diálogo "Salvar Como"
-    document.body.removeChild(a);           // Remover o link do DOM
-    URL.revokeObjectURL(url);               // Liberar a URL
-  }
-});
-
-// recebe um ID do form e devolve um JSON
-function getFormDataAsJson(formId) {
-  // Seleciona o formulário pelo ID
-  const form = document.getElementById(formId);
-
-  // Cria um objeto FormData a partir do formulário
-  const formData = new FormData(form);
-
-  // Inicializa um objeto vazio para armazenar os dados do formulário
-  const formDataJson = {};
-
-  // Itera sobre os pares chave/valor do FormData e adiciona ao objeto JSON
-  formData.forEach((value, key) => {
-    // Se a chave já existe, converte o valor em um array (para campos com o mesmo nome)
-    if (formDataJson[key]) {
-      if (Array.isArray(formDataJson[key])) {
-        formDataJson[key].push(value);
-      } else {
-        formDataJson[key] = [formDataJson[key], value];
-      }
-    } else {
-      formDataJson[key] = value;
-    }
-  });
-
-  // Retorna o objeto JSON
-  return formDataJson;
-};
-
-// funções de busca cep
-function meu_callback_estagiario(conteudo) {
-  if (!("erro" in conteudo)) {
-    //Atualiza os campos com os valores.
-    document.getElementById('enderecoEstagiario').value = (conteudo.logradouro);
-    document.getElementById('numEnderEstagiario').value = ("");
-    document.getElementById('bairroEstagiario').value = (conteudo.bairro);
-    document.getElementById('cidadeEstagiario').value = (conteudo.localidade);
-    document.getElementById('estadoEstagiario').value = (conteudo.uf);
-  } //end if.
-  else {
-    //CEP não Encontrado.
-    limpa_formulário_cep(1);
-    alert("CEP não encontrado.");
+    window.print();
   }
 }
-// funções de busca cep
-function meu_callback_empresa(conteudo) {
-  if (!("erro" in conteudo)) {
-    //Atualiza os campos com os valores.
-    document.getElementById('enderecoEmpresa').value = (conteudo.logradouro);
-    document.getElementById('numEnderEmpresa').value = ("");
-    document.getElementById('bairroEmpresa').value = (conteudo.bairro);
-    document.getElementById('cidadeEmpresa').value = (conteudo.localidade);
-    document.getElementById('estadoEmpresa').value = (conteudo.uf);
-  } //end if.
-  else {
-    //CEP não Encontrado.
-    limpa_formulário_cep(2);
-    alert("CEP não encontrado.");
-  }
-}
-// funções de busca cep
-function limpa_formulário_cep(quem) {
-  if (quem === 1) {
-    //Limpa valores do formulário de cep.
-    document.getElementById('enderecoEstagiario').value = ("");
-    document.getElementById('numEnderEstagiario').value = ("");
-    document.getElementById('bairroEstagiario').value = ("");
-    document.getElementById('cidadeEstagiario').value = ("");
-    document.getElementById('estadoEstagiario').value = ("");
-  } else {
-    //Limpa valores do formulário de cep.
-    document.getElementById('enderecoEmpresa').value = ("");
-    document.getElementById('numEnderEmpresa').value = ("");
-    document.getElementById('bairroEmpresa').value = ("");
-    document.getElementById('cidadeEmpresa').value = ("");
-    document.getElementById('estadoEmpresa').value = ("");
-  }
-}
-// funções de busca cep
-function pesquisaCep(valor, quem) { // 1 para estagiario 2 para empresa
-
-  //Nova variável "cep" somente com dígitos.
-  var cep = valor.replace(/\D/g, '');
-
-  //Verifica se campo cep possui valor informado.
-  if (cep != "") {
-
-    //Expressão regular para validar o CEP.
-    var validacep = /^[0-9]{8}$/;
-
-    //Valida o formato do CEP.
-    if (validacep.test(cep)) {
-
-      if (quem === 1) {
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('enderecoEstagiario').value = "...";
-        document.getElementById('numEnderEstagiario').value = ("...");
-        document.getElementById('bairroEstagiario').value = "...";
-        document.getElementById('cidadeEstagiario').value = "...";
-        document.getElementById('estadoEstagiario').value = "...";
-      } else {
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('enderecoEmpresa').value = "...";
-        document.getElementById('numEnderEmpresa').value = ("...");
-        document.getElementById('bairroEmpresa').value = "...";
-        document.getElementById('cidadeEmpresa').value = "...";
-        document.getElementById('estadoEmpresa').value = "...";
-      }
-
-      //Cria um elemento javascript.
-      var script = document.createElement('script');
-
-      //Sincroniza com o callback.
-      if (quem === 1) {
-        script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback_estagiario';
-      } else {
-        script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback_empresa';
-      }
-
-      //Insere script no documento e carrega o conteúdo.
-      document.body.appendChild(script);
-
-    } //end if.
-    else {
-      //cep é inválido.
-      limpa_formulário_cep(quem);
-      alert("Formato de CEP inválido.");
-    }
-  } //end if.
-  else {
-    //cep sem valor, limpa formulário.
-    limpa_formulário_cep(quem);
-  }
-};
 
 // devolve a data atual formatada
 // Saída: 10/07/2024
@@ -256,40 +91,21 @@ function getDataAtual() {
   return dia + '/' + mes + '/' + ano;
 };
 
-// Entrada: "2024-07-10";
-// Saída: 10/07/2024
-function formataData(data) {
-
-  // Dividindo a data original em componentes
-  let partes = data.split('-');
-  let ano = partes[0];
-  let mes = partes[1];
-  let dia = partes[2];
-
-  // Reformatando para o novo formato
-  let dataFormatada = `${dia}/${mes}/${ano}`;
-
-  return dataFormatada;
-};
-
-// busca um curso pelo id do curso
-// retorna um array [cursoID, cursoNome, profMatricula, profNome, profEmail]
-async function getCursoByID(siglaCurso) {
+async function buscarDados() {
+  const selecao = document.getElementById('curso');
+  const chaveSelecionada = selecao.value;
 
   try {
-    const resposta = await fetch(cursoURL);
+    const resposta = await fetch('./cursos.txt');
     const texto = await resposta.text();
 
     const linhas = texto.split('\n');
-    // Ignora a primeira linha
-    const linhasSemPrimeira = linhas.slice(1);
-
     let resultado = null;
 
     for (const linha of linhas) {
       const colunas = linha.split(',');
 
-      if (colunas[0] === siglaCurso) {
+      if (colunas[0] === chaveSelecionada) {
         resultado = colunas;
         break;
       }
@@ -305,120 +121,6 @@ async function getCursoByID(siglaCurso) {
     }
   } catch (erro) {
     console.error('Erro ao buscar dados:', erro);
-    alert("Erro ao buscar dados do curso");
-  }
-};
-
-// primeiro coloca os valores de formObject[<variavel>] em divPrint.innerHTML(${<variavel>})
-async function imprimir(formObject) {
-
-  const siglaCurso = formObject["siglaCurso"];
-
-  var result = await getCursoByID(siglaCurso);
-
-  if (result) {
-
-    formObject["nomeCurso"] = result[1];
-    formObject["matriculaProfessor"] = result[2];
-    formObject["nomeProfessor"] = result[3];
-    formObject["emailProfessor"] = result[4];
-
-    // Seleciona a div print
-    const divPrint = document.getElementById('print');
-
-    // Substitui os placeholders com os valores do objeto JSON
-    divPrint.innerHTML = divPrint.innerHTML
-      .replace('${nomeEstagiario}', formObject["nomeEstagiario"])
-      .replace('${nomeCurso}', formObject["nomeCurso"])
-      .replace('${matriculaEstagiario}', formObject["matriculaEstagiario"])
-      .replace('${cpfEstagiario}', formObject["cpfEstagiario"])
-      .replace('${telefoneEstagiario}', formObject["telefoneEstagiario"])
-      .replace('${emailEstagiario}', formObject["emailEstagiario"])
-      .replace('${dataNascimento}', await formataData(formObject["dataNascimento"]))
-      .replace('${enderecoCompletoEstagiario}', formObject["enderecoCompletoEstagiario"])
-      .replace('${deficiencia}', formObject["deficiencia"])
-      .replace('${nomeProfessor}', formObject["nomeProfessor"])
-      .replace('${matriculaProfessor}', formObject["matriculaProfessor"])
-      .replace('${emailProfessor}', formObject["emailProfessor"])
-      .replace('${nomeEmpresa}', formObject["nomeEmpresa"])
-      .replace('${cnpj}', formObject["cnpj"])
-      .replace('${enderecoCompletoEmpresa}', formObject["enderecoCompletoEmpresa"])
-      .replace('${telefoneEmpresa}', formObject["telefoneEmpresa"])
-      .replace('${emailEmpresa}', formObject["emailEmpresa"])
-      .replace('${ramoAtividade}', formObject["ramoAtividade"])
-      .replace('${representante}', formObject["representante"])
-      .replace('${supervidor}', formObject["supervidor"])
-      .replace('${cargo}', formObject["cargo"])
-      .replace('${apolice}', formObject["apolice"])
-      .replace('${seguradora}', formObject["seguradora"])
-      .replace('${cnpjSeguradora}', formObject["cnpjSeguradora"])
-      .replace('${dataInicio}', await formataData(formObject["dataInicio"]))
-      .replace('${dataFim}', await formataData(formObject["dataFim"]))
-      .replace('${dataAtual}', formObject["dataAtual"])
-      .replace('${nomeEstagiario2}', formObject["nomeEstagiario"])
-      .replace('${cpfEstagiario2}', formObject["cpfEstagiario"])
-      .replace('${tipoEstagio}', formObject["tipoEstagio"])
-      .replace('${dataInicio2}', await formataData(formObject["dataInicio"]))
-      .replace('${dataFim2}', await formataData(formObject["dataFim"]))
-      .replace('${horarioEstagio}', formObject["horarioEstagio"].replace(/\n/g, "<br>"))
-      .replace('${cargaHorariaSemanal}', formObject["cargaHorariaSemanal"])
-      .replace('${cargaHorariaTotal}', formObject["cargaHorariaTotal"])
-      .replace('${apolice2}', formObject["apolice"])
-      .replace('${bolsaAuxilio}', formObject["bolsaAuxilio"].replace(/\n/g, "<br>"))
-      .replace('${atividades}', formObject["atividades"].replace(/\n/g, "<br>"))
-
-    window.print();
+    alert("Erro ao buscar dados");
   }
 }
-
-// Adiciona um event listener para o evento de submissão do formulário
-document.getElementById('myForm').addEventListener('submit', function (event) {
-  // Impede o envio do formulário
-  event.preventDefault();
-
-  // pega dados do form
-  let formData = new FormData(event.target);
-
-  let formObject = {};
-
-  formData.forEach((value, key) => {
-    formObject[key] = value;
-  });
-
-  const estagEnder = formObject["enderecoEstagiario"] || ""
-  const estagNum = formObject["numEnderEstagiario"] || ""
-  const estagBairro = formObject["bairroEstagiario"] || ""
-  const estagCidade = formObject["cidadeEstagiario"] || ""
-  const estagEstado = formObject["estadoEstagiario"] || ""
-  const estagCep = formObject["cepEstagiario"] || ""
-
-  const empresaEnder = formObject["enderecoEmpresa"] || ""
-  const empresaNum = formObject["numEnderEmpresa"] || ""
-  const empresaBairro = formObject["bairroEmpresa"] || ""
-  const empresaCidade = formObject["cidadeEmpresa"] || ""
-  const empresaEstado = formObject["estadoEmpresa"] || ""
-  const empresaCep = formObject["cepEmpresa"] || ""
-
-  formObject["enderecoCompletoEstagiario"] =
-    estagEnder + ", " +
-    estagNum + " - " +
-    estagBairro + " - " +
-    estagCidade + "/" +
-    estagEstado + " CEP: " +
-    estagCep
-
-  formObject["enderecoCompletoEmpresa"] =
-    empresaEnder + ", " +
-    empresaNum + " - " +
-    empresaBairro + " - " +
-    empresaCidade + "/" +
-    empresaEstado + " CEP: " +
-    empresaCep
-
-  formObject["dataAtual"] = getDataAtual();
-
-  imprimir(formObject);
-
-});
-
-
