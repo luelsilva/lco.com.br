79cfd71f1e22b20ccff70ce184c0e0df741eabc1
bon4dev <bon4dev@gmail.com>
2025-02-10 12:42:42 -0300

    Update script.js

--- cedup/estagio/tce/script.js
+++ cedup/estagio/tce/script.js
@@ -3,42 +3,10 @@ const CURSOS_URL =
 
 const API_URL = "https://colupe.com/";
 
-const TCE_URL = "https://www.lco.com.br/cedup/estagio/tce/";
+const TCE_URL = "https://www.lco.com.br/cedup/estagio/tce/old/";
 
 const FAVICON_URL =
   "https://www.lco.com.br/cedup/estagio/assets/img/favicon.png";
-
-// onClick do elemento loadBtn
-document.getElementById("loadBtn").addEventListener("click", function () {
-  document.getElementById("fileInput").click();
-});
-
-// onChange do elemento fileInput
-document
-  .getElementById("fileInput")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        try {
-          const data = JSON.parse(e.target.result);
-          const form = document.getElementById("myForm");
-          for (const key in data) {
-            if (data.hasOwnProperty(key)) {
-              const input = form.querySelector(`[name="${key}"]`);
-              if (input) {
-                input.value = data[key];
-              }
-            }
-          }
-        } catch (error) {
-          alert("Erro ao processar o arquivo JSON: " + error.message);
-        }
-      };
-      reader.readAsText(file);
-    }
-  });
 
 // carrega cursos no form select 'siglaCurso'
 document.addEventListener("DOMContentLoaded", function () {
