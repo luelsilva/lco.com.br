6121119c98ac2777cb35f758de59884f69199b3a
bon4dev <bon4dev@gmail.com>
2025-03-01 23:00:56 -0300

    Update script.js

--- cedup/estagio/tce/script.js
+++ cedup/estagio/tce/script.js
@@ -60,7 +60,7 @@ function verificaParametrosUrl() {
         // Preenche os campos do formulário com os dados retornados
         Object.keys(data).forEach((key) => {
           const input = document.querySelector(`[name="${key}"]`);
-          console.log(`[name="${key}"]`);
+          console.log(key);
 
           if (input) {
             input.value = data[key];
